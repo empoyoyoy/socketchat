f8a273d0a3f08a7cdb26526f48efd8f1ccb724ae
unknown <jpgallarte@gmail.com>
2018-11-13 12:58:20 +0800

    make it better39

--- server/server.js
+++ server/server.js
@@ -6,12 +6,6 @@ const path = require('path');
 
 const DIST_FOLDER = path.join(process.cwd(), './dist');
 
-// app.use(express.static('./dist'));
- 
-// app.get('*', function(req,res) {
-//     res.sendFile(path.resolve('dist/socketchat/index.html'));
-// });
-
 app.get('*.*', express.static(path.join(DIST_FOLDER, 'socketchat')));
 
 app.get('*', (req, res) => {
