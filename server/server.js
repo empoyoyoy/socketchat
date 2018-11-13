bb6a41ee62c5e0c590c043d9e6476d02446af2aa
unknown <jpgallarte@gmail.com>
2018-11-13 10:33:47 +0800

    make it better15

--- server/server.js
+++ server/server.js
@@ -1,7 +1,35 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io  = require('socket.io')(http);
+// var app = require('express')();
+// var http = require('http').Server(app);
+// var io  = require('socket.io')(http);
 
+
+// io.on('connection', (socket)=> {
+
+//     console.log('User Connected...');
+    
+//     socket.on('disconnect', () => {
+//         console.log('User Disconnected...');
+//     });
+
+//     socket.on('add-message', (message, username)=>{
+//         io.emit('message', {type: 'new-message', text: message, username: username})
+//     })
+// });
+
+// http.listen('port', process.env.PORT || 5000, () => {
+//     console.log('server running on port 5000');
+// });
+
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io  = require('socket.io')(http);
+const path = require('path');
+
+app.use(express.static(path.join(__dirname,'dist')));
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname,'dist/socketchat/index.html'))
+})
 
 io.on('connection', (socket)=> {
 
@@ -16,7 +44,8 @@ io.on('connection', (socket)=> {
     })
 });
 
-http.listen('port', process.env.PORT || 5000, () => {
-    console.log('server running on port 5000');
-});
+const port = process.env.PORT || '5000';
+app.set('port',port);
 
+const server = http;
+server.listen(port,() => console.log('running hahahaha'));
