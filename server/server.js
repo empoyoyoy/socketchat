ccfcf6940ec2465d82f76781f4ea9fe46db6dc08
unknown <jpgallarte@gmail.com>
2018-11-13 14:53:02 +0800

    make it better51

--- server/server.js
+++ server/server.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const app = express();
-const http = require('http').createServer(app);
+const http = require('http').Server(app);
 const io  = require('socket.io')(http);
 const path = require('path');
 
 const DIST_FOLDER = path.join(process.cwd(), './dist');
 
+// app.use(express.static('./dist'));
+ 
+// app.get('*', function(req,res) {
+//     res.sendFile(path.resolve('dist/socketchat/index.html'));
+// });
+
 app.get('*.*', express.static(path.join(DIST_FOLDER, 'socketchat')));
 
 app.get('*', (req, res) => {
-    res.render(path.join(DIST_FOLDER, 'socketchat', 'index.html'), { req });
+    res.sendFile(path.join(DIST_FOLDER, 'socketchat', 'index.html'), { req });
   });
 
  
