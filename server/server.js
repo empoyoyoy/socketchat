975aa2f79ac03f0316803dce45a9d7b6c107053a
unknown <jpgallarte@gmail.com>
2018-11-13 13:04:42 +0800

    make it better40

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
-const http = require('http').Server(app);
+const http = require('http').createServer(app);
 const io  = require('socket.io')(http);
 const path = require('path');
 
-const DIST_FOLDER = path.join(process.cwd(), './dist');
+// const DIST_FOLDER = path.join(process.cwd(), './dist');
 
-app.get('*.*', express.static(path.join(DIST_FOLDER, 'socketchat')));
+// app.get('*.*', express.static(path.join(DIST_FOLDER, 'socketchat')));
 
-app.get('*', (req, res) => {
-    res.render(path.join(DIST_FOLDER, 'socketchat', 'index.html'), { req });
-  });
+// app.get('*', (req, res) => {
+//     res.render(path.join(DIST_FOLDER, 'socketchat', 'index.html'), { req });
+//   });
 
  
 io.on('connection', (socket)=> {
